28583b0c28a396f33584a9d7cca6d45ba19935e0
sonlv6 <vanson@cesoftvn.com>
2017-05-18 17:10:45 +0700

    'update'

--- resources/views/cat/indexmap.blade.php
+++ resources/views/cat/indexmap.blade.php
@@ -11,7 +11,17 @@
                             <thead>
                             <th>Name</th>
                             <th>Adress</th>
+                            <th>Phone</th>
+                            <th>Geometry</th>
+                            <th>Iternal phone</th>
+                            <th>Name</th>
+                            <th>Open hour</th>
                             <th>Rating</th>
+                            <th>Place id</th>
+                            <th>Review</th>
+                            <th>Scope</th>
+                            <th>Types</th>
+                            <th>URL</th>
                             <th>Delete</th>
                             <th></th>
                             </thead>
@@ -20,7 +30,17 @@
                                 <tr>
                                     <td>{{$data->name}}</td>
                                     <td>{{$data->vicinity}}</td>
+                                    <td>{{$data->phone}}</td>
+                                    <td>{{$data->geometry}}</td>
+                                    <td>{{$data->i_phone}}</td>
+                                    <td>{{$data->name}}</td>
+                                    <td>{{$data->openhour}}</td>
                                     <td>{{$data->rating}}</td>
+                                    <td>{{$data->place_id}}</td>
+                                    <td>{{$data->review}}</td>
+                                    <td>{{$data->scope}}</td>
+                                    <td>{{$data->types}}</td>
+                                    <td>{{$data->url}}</td>
                                     <td><a href="{{route('mdelete',$data->id)}}" data-placement="top" data-toggle="tooltip" title="Delete"><button class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-trash"></span></button></a></td>
                                 </tr>
                             @endforeach
